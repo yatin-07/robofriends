93ec657c6ef899f9bc83989f9b83727b89ea7237
itprogaur <professionalcodehub@gmail.com>
2021-12-31 18:37:07 +0530

    Card appear

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = (name, email, id) =>{
+const Card = ({name, email, id}) =>{
 
     return(
         <div className='bg-light-green dib br3 pa2 ma2 grow bw2 shadow-5 tc'>
             <img src={`https://robohash.org/${id}?200x200`} alt='photo'/>
 
-        <div className=''>
+        <div>
             <h2>{name}</h2>
             <p>{email}</p>
         </div>
